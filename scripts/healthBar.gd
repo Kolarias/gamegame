76afa521da602a7c28ade35055442b3a85c4f2a3
kartoffeln2112 <62444855+kartoffeln2112@users.noreply.github.com>
2020-05-30 16:26:36 -0500

    controls the GUI health bar for the player
    version only receives "hp_change" signal from player
    "death" signal should later be implemented to set the gui to invisible

---
+++ scripts/healthBar.gd
@@ -0,0 +1,16 @@
+extends TextureProgress
+
+
+# Declare member variables here. Examples:
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+#receives signal from player function, changes value of hp
+func _on_Player_hp_change(player):
+	value = player.hp
